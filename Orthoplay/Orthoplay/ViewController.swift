933a408f5ee330fc7a898cf51c07cebb34e98dcc
Isak Dunér <isak.duner@gmail.com>
2016-03-29 15:37:15 +0200

    add communicate volume from app to watch

--- Orthoplay/Orthoplay/ViewController.swift
+++ Orthoplay/Orthoplay/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate {
 
     @IBOutlet weak var volumeDisplay: UILabel!
+    var session : WCSession!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if WCSession.isSupported() {
+            session = WCSession.defaultSession()
+            session.delegate = self
+            session.activateSession()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,9 +31,55 @@ class ViewController: UIViewController {
     }
 
     @IBAction func changeVolume(sender: AnyObject) {
-        var t:float_t
-        t = sender.value
-        self.volumeDisplay.text = String(format: "%.0f", t)
+        var volume:float_t
+        volume = sender.value
+        let volumeString = String(format: "%.0f", volume)
+        
+        //Set volume display
+        self.volumeDisplay.text = volumeString
+        
+        //Send to watch
+        sendToWatch(volumeString)
+        
+    }
+    
+    // Method to send message to watchOS
+    func sendToWatch(message: String) {
+        // A dictionary of property list values that you want to send.
+        let messageToSend = ["Message":message]
+        
+        // Task : Sends a message immediately to the counterpart and optionally delivers a response
+        session.sendMessage(messageToSend, replyHandler: { (replyMessage) -> Void in
+            
+            // Reply handler - present the reply message on screen
+            let value = replyMessage["Message"] as? String
+            
+            // GCD - Present on the screen
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                //self.replyLabel.text = value!
+            })
+            
+        }) { (error:NSError) -> Void in
+            // Catch any error Handler
+            print("error: \(error.localizedDescription)")
+        }
+    }
+    
+    // WCSession Delegate protocol
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject], replyHandler: ([String : AnyObject]) -> Void) {
+        
+        // Reply handler, received message
+        let value = message["Message"] as? String
+        
+        // GCD - Present on the screen
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+            //self.messageLabel.setText(value!)
+            self.volumeDisplay.text = value
+        }
+        
+        // Send a reply
+        //replyHandler(["Message":"Yes!\niOS 9.0 + WatchOS2 ..AAAAAAmazing!"])
+        
     }
 
 }
--- Orthoplay/Orthowatch Extension/InterfaceController.swift
+++ Orthoplay/Orthowatch Extension/InterfaceController.swift
@@ -8,10 +8,14 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
 
+    @IBOutlet var volumeDisplay: WKInterfaceLabel!
+    var session : WCSession!
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
@@ -21,11 +25,34 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        // To configure and activate the session
+        if WCSession.isSupported() {
+            session = WCSession.defaultSession()
+            session.delegate = self
+            session.activateSession()
+        }
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    // WCSession Delegate protocol
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject], replyHandler: ([String : AnyObject]) -> Void) {
+        
+        // Reply handler, received message
+        let value = message["Message"] as? String
+        
+        // GCD - Present on the screen
+        dispatch_async(dispatch_get_main_queue()) { () -> Void in
+            self.volumeDisplay.setText(value)
+        }
+        
+        // Send a reply
+        //replyHandler(["Message":"Yes!\niOS 9.0 + WatchOS2 ..AAAAAAmazing!"])
+        
     }
 
 }
